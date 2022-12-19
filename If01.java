f84c8fc47e4b94fbc2db2926513c4e94c0eefe50
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-19 11:20:59 +0000

    条件式の使い方の学習記録

---
+++ If01.java
@@ -0,0 +1,20 @@
+public class If01 {
+	public static void main(String[] args) {
+		int number = 7;
+		if (number < 5) {
+			System.out.println(number + " は5未満です");
+		}
+		if (number < 10) {
+			System.out.println(number + " は10未満です");
+		}
+		if (number < 15) {
+			System.out.println(number + " は15未満です");
+		}
+	}
+}
+
+// 実行結果
+/*
+7 は10未満です
+7 は15未満です
+*/ 
