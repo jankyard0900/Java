1a1b121a9fb63f95015f53b355afbee43fe03d89
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-19 08:12:26 +0000

    定数についての演習

---
+++ Chapter04.java
@@ -0,0 +1,17 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    final int LUCKY_NUMBER = 7;
+    String str = "好きな言葉";
+    System.out.println(str);
+    str = "少年よ大志を抱け";
+    System.out.println(str);
+    System.out.println(LUCKY_NUMBER);
+  }
+}
+
+//実行結果
+/*
+好きな言葉
+少年よ大志を抱け
+7
+*/
