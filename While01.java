7d50188925d191891e43f2312784831d6e8be9f5
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-20 08:55:02 +0000

    while文の学習記録

---
+++ While01.java
@@ -0,0 +1,19 @@
+public class While01 {
+  public static void main(String[] args) {
+	  int number = 1;
+	  while (number < 50) {
+		  number *= 2;
+		  System.out.println("While01 =" + number);
+	  }
+  }
+}
+
+// 実行結果
+/*
+While01 =2
+While01 =4
+While01 =8
+While01 =16
+While01 =32
+While01 =64
+*/ 
