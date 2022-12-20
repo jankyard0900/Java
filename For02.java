ccba3b99582d3c120cf19d3a7003b9342f8ed526
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-20 10:05:48 +0000

    for拡張文の学習記録

---
+++ For02.java
@@ -0,0 +1,17 @@
+public class For02 {
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		for (int number : array) {
+			System.out.println("For02 = " + number);
+		}
+	}
+}
+
+// 実行結果
+/*
+For02 = 1
+For02 = 2
+For02 = 3
+For02 = 4
+For02 = 5
+*/ 
