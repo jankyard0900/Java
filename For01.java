985f88164a0c1b977bb9c1da06cac582c00a6d51
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-20 09:35:41 +0000

    for文の学習記録

---
+++ For01.java
@@ -0,0 +1,16 @@
+public class For01 {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 5; number++) {
+            System.out.println("For01 = " + number);
+        }
+    }
+}
+
+// 実行結果
+/*
+For01 = 1
+For01 = 2
+For01 = 3
+For01 = 4
+For01 = 5
+*/ 
