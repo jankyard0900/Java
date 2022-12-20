9cf227674e0cf9d2a2b6df61864bb9fb94a487de
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-20 08:57:39 +0000

    プログラムの説明追加

--- While01.java
+++ While01.java
@@ -1,11 +1,15 @@
 public class While01 {
-  public static void main(String[] args) {
-	  int number = 1;
-	  while (number < 50) {
-		  number *= 2;
-		  System.out.println("While01 =" + number);
-	  }
-  }
+    public static void main(String[] args) {
+        int number = 1;
+        // while文：ループ開始
+        // numberが50未満の場合、ブロックの処理を行なう
+        while (number < 50) {
+            // ブロック：
+            // numberにnumber * 2 を代入して、numberを出力
+            number *= 2;
+            System.out.println("While01 = " + number);
+        }
+    }
 }
 
 // 実行結果
