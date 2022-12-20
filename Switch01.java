d47813ada4634d1bf774e6639d089dfcae4cc938
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-20 08:31:11 +0000

    Switch文の使い方の学習記録

---
+++ Switch01.java
@@ -0,0 +1,20 @@
+public class Switch01 {
+  public static void main(Strting[] args) {
+	  int val = 2;
+	  switch (val) {
+	  case 3:
+		  System.out.println("よくできました！");
+		  break;
+	  case 2:
+		  System.out.println("ふつうでした！");
+	      break;
+	  default:
+		  System.out.println("がんばろう！");
+	  }
+  } 
+}
+
+// 実行結果
+/*
+ふつうでした！
+*/ 
