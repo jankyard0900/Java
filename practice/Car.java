a3dafb9b13b9121387abb438560bfc297a93e48e
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-21 07:53:25 +0000

    パッケージとメソッドについて学習したからその問題

---
+++ practice/Car.java
@@ -0,0 +1,24 @@
+// 「Chapter09.java」を実行した結果が以下になるように「Car.java」を作成
+// 実行結果
+/*
+20 km 走ります
+残量は、53.4です
+*/ 
+
+
+package practice;
+
+public class Car {
+    // 燃費（Km/L）
+	private double fuelCost;
+	// 残量（L）
+	private double fuelAmount;
+	
+	// コンストラクタを作成
+	
+	// moveメソッド
+	// ・"xx km 走ります"を出力
+	// ・残量を計算
+	
+	// fuelAmountを
+}
