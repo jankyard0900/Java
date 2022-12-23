0a8b440fdc46e15e85612820954fe59c0f96a780
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-23 12:54:35 +0000

    例外の問題回答・解説

--- practice/Chapter10.java
+++ practice/Chapter10.java
@@ -13,7 +13,7 @@ java.sql.SQLException: SQLエラーです
 throwSQLExceptionの呼び出し終了
 mainメソッド終了
 
-*/ 
+*/
 
 // ※「xxxx」は、プログラムの行番号が出力される
 
@@ -26,12 +26,14 @@ public class Chapter10 {
 
 		try {
 			int array[] = { 1, 3, 5 };
-			Chapter10.validTndex(array, 2);
+			Chapter10.validIndex(array, 2);
 			Chapter10.validIndex(array, 3);
 
+			// Exceptionが発生しない場合は出力される
 			System.out.println("validIndexの呼び出し終了");
 		} catch (IllegalArgumentException e) {
 			System.out.println("IllegalArgumentExpectionが発生しました");
+			// スタックトレースを出力
 			e.printStackTrace();
 		}
 
@@ -39,6 +41,16 @@ public class Chapter10 {
 		// ・throwSQLExceptionメソッドを呼び出し
 		// ・SQL Exceptionをキャッチしてメッセージとスタットレースを出力
 		// ・"throwSQLExceptionの呼び出し終了"のメッセージを出力
+	    try {
+	    	Chapter10.throwSQLException();
+	    } catch (SQLException e) {
+	    	System.out.println("SQLExceptionが発生しました");
+	    	// printlnStaclTraceでスタックトレースを出力
+	    	e.printStackTrace();
+	    } finally {
+	    	// 例外発生しても出力するため、finallyブロックに記述
+	    	System.out.println("throwSQLExceptionの呼び出し終了");
+	    }
 
 		System.out.println("mainメソッド終了");
 	}
@@ -46,8 +58,18 @@ public class Chapter10 {
 	// validIndexメソッドを作成
 	// ・indexがarrayのサイズの範囲内なら、インデックスの要素を出力
 	// 。サイズの範囲外なら、IllegalArgumentExceptionをスロー
+	public static void validIndex(int[] array, int index) {
+		// 配列のサイズは、lebgthで取得
+		if (array.length <= index) {
+			// throw文で、例外をスロー
+			throw new IllegalArgumentException(index + " はサイズの範囲外です");
+		}
+		System.out.println("インデックス" + index + " の要素は " + array[index] + "です");
+	}
 
+	// スローされる例外が検査例外なので、throws分が必要
 	public static void throwSQLException() throws SQLException {
+		// SQLExceptionは、通常、SQLでエラーの場合に発生する例外
 		throw new SQLException("SQLエラーです");
 	}
 }
