7304ee2255d11b97665e9066bd792ac5ed9460bd
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-24 07:11:48 +0000

    カプセル化の問題解答

--- practice/CapsuleMan.java
+++ practice/CapsuleMan.java
@@ -2,13 +2,25 @@
 
 package practice;
 
-public class NoCapsuleMan {
-    public String name;
-    public int age;
+public class CapsuleMan {
+    private String name;
+    private int age;
 
-    public NoCapsuleMan(String name, int age) {
+    public CapsuleMan(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
     }
 }
 
