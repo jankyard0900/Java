a3695314f2daf39123d82f73c1dbf6c71d14acd7
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-26 07:24:19 +0000

    インフェースについて学んだためその記録

---
+++ extend/Interface01.java
@@ -0,0 +1,25 @@
+package extend;
+
+import extend.phone.Camera;
+import extend.phone.MobilePhone;
+
+public class Interface01 {
+    public static void main(String[] args) {
+        MobilePhone mobilePhone = new MobilePhone("000-1111-2222");
+
+        System.out.println("電話番号は最大" + MobilePhone.MAX_NUMBER_DIGITS + "桁です。");
+        mobilePhone.call("99-8888-7777");
+        Interface01.classTakePicture(mobilePhone);
+    }
+
+    public static void classTakePicture(Camera camera) {
+        camera.takePicture();
+    }
+}
+
+// 実行結果
+/*
+電話番号は最大11桁です。
+000-1111-2222 から 99-8888-7777 に電話をかけます。
+写真をとります。
+*/
