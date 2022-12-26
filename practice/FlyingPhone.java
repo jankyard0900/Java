d21c894f13357914aed2808bcf84f04ed4ab83c6
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-26 08:32:06 +0000

    継承のまとめ問題解答

--- practice/FlyingPhone.java
+++ practice/FlyingPhone.java
@@ -18,5 +18,22 @@ package practice;
 import extend.phone.Phone;
 
 public class FlyingPhone implements Phone, Flying {
-    // プログラムを作成
+	// プログラムを作成
+	private int minutes;
+
+	public FlyingPhone(int minutes) {
+		this.minutes = minutes;
+	}
+
+	public void call(String number) {
+		System.out.println(number + " に電話します。通話できるのは飛んでる間だけです。");
+	}
+
+	public void fly() {
+		System.out.println(minutes + " 分間、飛びます。 ");
+	}
+
+	public void powerOff() {
+		Flying.super.powerOff();
+	}
 }
