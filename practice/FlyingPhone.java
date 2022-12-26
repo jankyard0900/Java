fd63c411b63265a5b55f9db863ca9c36b8703ba7
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-26 08:48:33 +0000

    継承のまとめ問題の解説

--- practice/FlyingPhone.java
+++ practice/FlyingPhone.java
@@ -19,21 +19,26 @@ import extend.phone.Phone;
 
 public class FlyingPhone implements Phone, Flying {
 	// プログラムを作成
-	private int minutes;
+  private int minutes;
+  
+  // コンストラクタ。引数でメンバ変数を初期化。
+  public FlyingPhone(int minutes) {
+      this.minutes = minutes;
+  }
 
-	public FlyingPhone(int minutes) {
-		this.minutes = minutes;
-	}
+  // Phoneインターフェースのメソッドをオーバーライド
+  public void call(String number) {
+      System.out.println(number + " に電話します。通話できるのは飛んでいる間だけです。");
+  }
+  
+  // Flyingインターフェースのメソッドをオーバーライド
+  public void fly() {
+      System.out.println(minutes + " 分間、飛びます。 ");
+  }
 
-	public void call(String number) {
-		System.out.println(number + " に電話します。通話できるのは飛んでる間だけです。");
-	}
-
-	public void fly() {
-		System.out.println(minutes + " 分間、飛びます。 ");
-	}
-
-	public void powerOff() {
-		Flying.super.powerOff();
-	}
+  // オーバーライドしないとコンパイルエラー
+  public void powerOff() {
+      // Flyingのdefaultメソッドを呼び出す
+      Flying.super.powerOff();
+  }
 }
