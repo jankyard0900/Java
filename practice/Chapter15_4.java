a9d5ad60d0cd2977851d7e9c78a6f6a0d25f82aa
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-12-28 07:47:02 +0000

    スレッドの問題作成

---
+++ practice/Chapter15_4.java
@@ -0,0 +1,41 @@
+// 以下の実行結果になるように、「Chapter15_4.java」と「ThreadSample.java」を作成する。
+// 実行結果 ※出力される順序は実行するごとに変わる
+/*
+ThreadSample2のスレッド : 1回目
+Chapter15_4のスレッド : 1回目
+ThreadSample1のスレッド : 1回目
+ThreadSample2のスレッド : 2回目
+ThreadSample1のスレッド : 2回目
+Chapter15_4のスレッド : 2回目
+ThreadSample2のスレッド : 3回目
+Chapter15_4のスレッド : 3回目
+ThreadSample1のスレッド : 3回目
+*/ 
+
+package practice;
+
+public class Chapter15_4 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 2; i++) {
+			// ThreadSampleクラスのインスタンスを生成
+			・・・
+			// Threadクラスのインスタンスを作成
+			・・・
+			// Threadクラスのstartメソッドを呼び出す
+			・・・
+		}
+		
+		for (int i = 1; i <= 3; i++) {
+			try {
+				// Thread.sleepは、指定したミリ秒間スリープするメソッド
+				Thread.sleep(1000);
+				// 1000ミリ秒（1秒）間スリープしてメッセージを表示
+				System.out.println("Chapter15_4のスレッド：" + i + "回目");
+			// Thread.sleepの例外処理
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
