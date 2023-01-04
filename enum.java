678f29e59a75699d97fd28ca25562cc7652cf6b5
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-04 07:14:02 +0000

    列挙型の基本を学んだので記録

---
+++ enum.java
@@ -0,0 +1,14 @@
+public enum Dessert {
+    APPLE, ICE_CREAME, CAKE,
+}
+
+System.out.println(Dessert.APPLE);
+System.out.println(Dessert.ICE_CREAM);
+System.out.println(Dessert.CAKE);
+
+// 実行結果
+/*
+APPLE
+ICE_CREAM
+
+*/ 
