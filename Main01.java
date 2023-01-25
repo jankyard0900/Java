a5635ccbf59cea1eb002aea06e6b9b85950c09f1
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-25 07:40:22 +0000

    メソッドを使ってみよう

---
+++ Main01.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    hello();
+  }
+  
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
+  }
+}
+
+// 実行結果
+// Hello Java
