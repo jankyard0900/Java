1a05e151e56f7e9530f9229b44c45eac67aa80a3
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-26 08:54:39 +0000

    メソッドの定義〜呼び出しまでの説明を追加で記入

--- Main01.java
+++ Main01.java
@@ -1,8 +1,10 @@
 class Main {
   public static void main(String[] args) {
+    // helloメソッドの呼び出し
     hello();
   }
   
+  // helloメソッドを定義してください
   public static void hello() {
     // "Hello World"を、"Hello Java"に書き換えてください
     System.out.println("Hello Java");
