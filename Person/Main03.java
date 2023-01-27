c06eac513b27d435d033e6e8b9100f2593aa01dc
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-27 05:57:46 +0000

    Main03クラスは実行用のクラス、Person01クラスはロジックをまとめるクラスとして分けました

---
+++ Person/Main03.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    Person01.printData(Person01.fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    Person01.printData(Person01.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
+  }
+}
+
+// 実行結果
+/*
+私の名前はKate Jonesです
+年齢は27歳です
+身長は1.6mです
+体重は50.0kgです
+BMIは19.53125です
+健康です
+私の名前はJohn Christopher Smithです
+年齢は65歳です
+身長は1.75mです
+体重は80.0kgです
+BMIは26.122448979591837です
+健康ではありません
+*/
