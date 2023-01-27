bd452867e18b784f282281aedaf7a39068872084
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-27 04:37:49 +0000

    戻り値のあるメソッドについて学習したので記録

--- Main02.java
+++ Main02.java
@@ -1,7 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    // それぞれ年齢に関する引数を追加してください
+    // fullNameメソッドの結果を変数nameに代入してください
+    String name = fullName("Kate", "Jones");
+    
+    // printDataの引数を書き換えてください
     printData("Kate Jones", 27);
+    
+    // こちらは書き換えないでください
     printData("John Christopher", 65);
   }
   
@@ -10,6 +15,12 @@ class Main {
     System.out.println("私の名前は" + name + "です");
     // 「年齢は◯◯歳です」と出力してください
     System.out.println("年齢は" + age + "歳です");
+    
+  // fullNameメソッドを定義してください
+  public static String fullName(String firstName, String lastName) {
+    // 呼び出し元に値を返す戻り値設定
+    return firstName + " " + lastName;
+  }
   }
 }
 
