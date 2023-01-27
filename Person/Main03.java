882f7e5e2ce19a3bf3f51e76f99152c64e965a82
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-27 06:24:34 +0000

    数値の入力を受けとる方法と出力

--- Person/Main03.java
+++ Person/Main03.java
@@ -1,11 +1,35 @@
+// java.util.Scannerを読み込んでください
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    Person01.printData(Person01.fullName("Kate", "Jones"), 27, 1.6, 50.0);
-    Person01.printData(Person01.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("名前：");
+    // 文字列の入力を受け取ってください
+    String firstName = scanner.next();
+    
+    System.out.print("名字：");
+    // 文字列の入力を受け取ってください
+    String lastName = scanner.next();
+    
+    System.out.print("年齢：");
+    // 整数の入力を受け取ってください
+    int age = scanner.nextInt();
+    
+    System.out.print("身長(m)：");
+    // 小数の入力を受け取ってください
+    double height = scanner.nextDouble();
+    
+    System.out.print("体重(kg)：");
+    // 小数の入力を受け取ってください
+    double weight = scanner.nextDouble();
+    
+    Person01.printData(Person01.fullName(firstName, lastName), age, height, weight);
   }
 }
 
-// 実行結果
+// 実行結果（入力したオブジェクトが入ります）
 /*
 私の名前はKate Jonesです
 年齢は27歳です
